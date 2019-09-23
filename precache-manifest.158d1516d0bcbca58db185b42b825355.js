24a23bd5f11301083a028b18f20ef067e8f22c9d
salmanyaqoobin <34335865+salmanyaqoobin@users.noreply.github.com>
2019-09-23 18:26:38 +0300

    Updates

---
+++ precache-manifest.158d1516d0bcbca58db185b42b825355.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fba0eaf4e311b13cd45e77260c13214d",
+    "url": "/robofriends-redux/index.html"
+  },
+  {
+    "revision": "4d32b1a2976aba58b40c",
+    "url": "/robofriends-redux/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "4a3aec9e4bc83a43c2dd",
+    "url": "/robofriends-redux/static/css/main.e14962de.chunk.css"
+  },
+  {
+    "revision": "4d32b1a2976aba58b40c",
+    "url": "/robofriends-redux/static/js/2.7dea1a5c.chunk.js"
+  },
+  {
+    "revision": "4a3aec9e4bc83a43c2dd",
+    "url": "/robofriends-redux/static/js/main.bf099bd4.chunk.js"
+  },
+  {
+    "revision": "f6678c7a329d9aea8e00",
+    "url": "/robofriends-redux/static/js/runtime~main.1040082c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-redux/static/media/SEGA.12a3db8b.woff"
+  }
+]);
